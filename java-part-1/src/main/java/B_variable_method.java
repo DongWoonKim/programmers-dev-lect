7b12e1f851fa3ca9f4e8efb67460d5061f104ca2
DongWoonKim <01030985843z@naver.com>
2026-06-04 12:26:13 +0900

    variable function

--- java-part-1/src/main/java/B_variable_method.java
+++ java-part-1/src/main/java/B_variable_method.java
@@ -40,8 +40,15 @@
 // 변수 타입 앞에 'final'을 붙여주면 된다.
 
 public class B_variable_method {
-    static void main(String[] args) {
 
+    // * 함수 : 프로그래밍에서 특정 작업을 수행하기 위해 작성된 코드의 묶음
+    /*
+        1. 함수 선언 : 함수의 이름과 특성을 정의하는 부분
+        2. 매개 변수(파라미터) : 매개변수는 함수가 작업을 수행하는 데 필요한 입력값을 전달받는 부분이다.
+        3. 반환 타입 : 함수가 어떤 유형의 값을 반환할지를 정의
+     */
+    public static void exam1() {
+        // 수행할 내용
         byte myByte = 127;
         System.out.println("myByte : " + myByte);
 
@@ -80,6 +87,37 @@ public class B_variable_method {
         // 문자열 -> 참조형
         String str = "Hello World";
         System.out.println("str : " + str);
+    }
+
+    public static int add(int a, int b) {
+//        int result = a + b;
+//        return result;
+        return a + b;
+    }
+
+    // minus
+    // 20, 10 -> 정수반환
+    public static int minus(int a, int b) {
+        return a - b;
+    }
+
+    // 결과만 출력하는 함수 printResult를 완성해주세요
+    public static void printResult(int result) {
+        System.out.println("result : " + result);
+        return;
+        /*
+            * return
+            * 1. 값을 반환 : 함수가 어떤 값을 계산하거나 처리한 후, 그 결과를 호출한 코드로 돌려줄 때 사용
+            * 2. 함수 종료 : return 문이 실행되면 행당 함수는 즉시 종료된다.
+         */
+    }
+
+
+    static void main(String[] args) {
+        int result = add(10, 20);
+        printResult(result);
+        result = minus(20, 10);
+        printResult(result);
 
     }
 }
