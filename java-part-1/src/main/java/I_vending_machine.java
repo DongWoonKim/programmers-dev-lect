f052c8da90ea808c119c5833c2ef02ebd863fe1e
DongWoonKim <01030985843z@naver.com>
2026-06-05 11:48:43 +0900

    switch case

---
+++ java-part-1/src/main/java/I_vending_machine.java
@@ -0,0 +1,18 @@
+public class I_vending_machine {
+
+    // [요구사항]
+    // 사용자 메뉴가 출력
+    // 돈 넣기
+    // 메뉴선택 시 -> 음료가 나온다, 돈이 차감된다.
+    // 종료시 잔돈이 반환된다.
+
+    static void main(String[] args) {
+        int totalMoney = 0;
+
+        while ( true ) {
+
+        }
+
+    }
+
+}
