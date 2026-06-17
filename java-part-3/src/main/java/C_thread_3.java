9d3d4cb2a3fa9b4d02a0cfb19dadc57d592d774b
DongWoonKim <01030985843z@naver.com>
2026-06-17 10:04:17 +0900

    java-part-3 C

--- java-part-3/src/main/java/C_thread_3.java
+++ java-part-3/src/main/java/C_thread_3.java
@@ -1,6 +1,11 @@
 
 // * 싱글스레드 vs 멀티스레드
 
+// - 멀티스레드가 항상 더 빠른 건 아니다. 오히려 더 느릴 수도 있다.
+// - 그래서 단순 계산만 하는 작업이라면 싱글스레드가 더 효율적일 수 있다.
+// 스레드를 번갈아 바꾸는 데 드는 비용(= 작업 전환, 컨택스트 스위칭) 때문이다.
+// - 멀티 스레드가 유리한 경우 : 서로 다른 자원을 쓰는 작업
+
 class C_thread_3_1 extends Thread{
     @Override
     public void run() {
@@ -45,6 +50,8 @@ public class C_thread_3 {
     }
 
     static void main(String[] args) {
+
+        exam2();
 
     }
 }
