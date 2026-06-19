03eb2dd0078a45a691ca6d0ede4604d9ec0fd897
DongWoonKim <01030985843z@naver.com>
2026-06-19 11:17:15 +0900

    java-part-3 A

--- java-part-4/src/main/java/A_jdbc.java
+++ java-part-4/src/main/java/A_jdbc.java
@@ -1,5 +1,6 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class A_jdbc {
@@ -23,7 +24,29 @@ public class A_jdbc {
         }
     }
 
+    public void insertData(String name, int age, String phone) {
+        // sql injection 조사
+        String query = "INSERT INTO member (name, age, phone) VALUES (?, ?, ?)";
+
+        try (
+                Connection conn = connection();
+                PreparedStatement pstmt = conn.prepareStatement( query );
+        ) {
+
+            pstmt.setString(1, name);
+            pstmt.setInt(2, age);
+            pstmt.setString(3, phone);
+
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
     static void main(String[] args) {
+        A_jdbc aJdbc = new A_jdbc();
+        aJdbc.connection();
 
     }
 }
