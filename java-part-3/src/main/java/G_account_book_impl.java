df4780e7d4524edc62278de9c1349a33289b41ac
DongWoonKim <01030985843z@naver.com>
2026-06-19 09:26:07 +0900

    java-part-3 G

--- java-part-3/src/main/java/G_account_book_impl.java
+++ java-part-3/src/main/java/G_account_book_impl.java
@@ -1,7 +1,20 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class G_account_book_impl implements G_account_book {
+
+    private final String DIR = System.getProperty("user.home") + File.separator + "Desktop" + File.separator + "/accountbook";
+    private Scanner sc;
+
+    public G_account_book_impl(Scanner sc) {
+        this.sc = sc;
+        File folder = new File(DIR);
+        if (!folder.exists()) folder.mkdir(); // 폴더 생성
+    }
+
     @Override
     public void addAccount() {
-        
+
     }
 
     @Override
