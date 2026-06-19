73c76d089ea71c279eff276efda29eec6b7831e9
DongWoonKim <01030985843z@naver.com>
2026-06-19 09:15:55 +0900

    java-part-3 G

---
+++ java-part-3/src/main/java/G_account_book_impl.java
@@ -0,0 +1,16 @@
+public class G_account_book_impl implements G_account_book {
+    @Override
+    public void addAccount() {
+        
+    }
+
+    @Override
+    public void showAccount() {
+
+    }
+
+    @Override
+    public void deleteAccount() {
+
+    }
+}
---
+++ java-part-3/src/main/java/G_start.java
@@ -0,0 +1,5 @@
+public class G_start {
+    static void main(String[] args) {
+
+    }
+}
