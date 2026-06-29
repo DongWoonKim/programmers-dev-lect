dcf2598171c6f8a19c53c9a1b342573e0eb165b2
DongWoonKim <01030985843z@naver.com>
2026-06-29 11:29:48 +0900

    spring theory ch05

--- spring-theory/src/main/java/com/example/spring/springtheory/ch05/ex_5_1/dao/UserDAO.java
+++ spring-theory/src/main/java/com/example/spring/springtheory/ch05/ex_5_1/dao/UserDAO.java
@@ -7,6 +7,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 public class UserDAO {
 
@@ -25,8 +26,11 @@ public class UserDAO {
             user.setId( rs.getString("id") );
             user.setName( rs.getString("name") );
             user.setPassword( rs.getString("password") );
-            user.
-            return null;
+            user.setLevel( Level.valueOf(rs.getInt("level")) );
+            user.setLogin( rs.getInt("login") );
+            user.setRecommand( rs.getInt("recommand") );
+
+            return user;
         }
     };
 
@@ -36,12 +40,15 @@ public class UserDAO {
             @Override
             public PreparedStatement makeStatement(Connection conn) throws SQLException {
                 PreparedStatement pstmt = conn.prepareStatement(
-                        "INSERT INTO users(id, name, password) VALUES(?, ?, ?)"
+                        "INSERT INTO users(id, name, password, level, login, recommend) VALUES(?, ?, ?, ?, ?, ?)"
                 );
 
                 pstmt.setString(1, user.getId());
                 pstmt.setString(2, user.getName());
                 pstmt.setString(3, user.getPassword());
+                pstmt.setInt(4, user.getLevel().getValue());
+                pstmt.setInt(5, user.getLogin());
+                pstmt.setInt(6, user.getRecommand());
 
                 return pstmt;
             }
@@ -60,6 +67,31 @@ public class UserDAO {
         };
 
         jdbcContext.workWithStatementStrategy(strategy);
+    }
+
+    public User get(String id) throws SQLException, ClassNotFoundException {
+        StatementStrategy strategy = new StatementStrategy() {
+            @Override
+            public PreparedStatement makeStatement(Connection conn) throws SQLException {
+                PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM users WHERE id = ?");
+                pstmt.setString(1, id);
+                return pstmt;
+            }
+        };
+
+        return jdbcContext.queryForObject( strategy, userRowMapper );
+    }
+
+    public List<User> getAll() throws SQLException, ClassNotFoundException {
+        StatementStrategy strategy = new StatementStrategy() {
+            @Override
+            public PreparedStatement makeStatement(Connection conn) throws SQLException {
+                PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM users ORDER BY id");
+                return pstmt;
+            }
+        };
+
+        return jdbcContext.query(strategy, userRowMapper);
     }
 
 
