5581a8210d9e8e92cfb0d5cf5c47524cacd11396
DongWoonKim <01030985843z@naver.com>
2026-06-29 09:08:44 +0900

    spring theory ch03

---
+++ spring-theory/src/main/java/com/example/spring/springtheory/ch03/ex_3_4/dao/UserDAO.java
@@ -0,0 +1,62 @@
+package com.example.spring.springtheory.ch03.ex_3_4.dao;
+
+
+import com.example.spring.springtheory.ch03.ex_3_4.domain.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class UserDAO {
+
+    private SimpleConnectionMaker simpleConnectionMaker;
+
+    public UserDAO(SimpleConnectionMaker simpleConnectionMaker) {
+        this.simpleConnectionMaker = simpleConnectionMaker;
+    }
+
+    protected UserDAO() {}
+
+    public void jdbcContextWithStatementStrategy(StatementStrategy statementStrategy) throws SQLException, ClassNotFoundException {
+        try (
+                Connection conn = simpleConnectionMaker.makeNewConnection();
+                PreparedStatement pstmt = statementStrategy.makeStatement(conn); // 변하는 부분을 전략에 위임
+        ) {
+            pstmt.executeUpdate();
+        }
+    }
+
+    public void add(User user) throws ClassNotFoundException, SQLException {
+
+        StatementStrategy strategy = new StatementStrategy() {
+            @Override
+            public PreparedStatement makeStatement(Connection conn) throws SQLException {
+                PreparedStatement pstmt = conn.prepareStatement(
+                        "INSERT INTO users(id, name, password) VALUES(?, ?, ?)"
+                );
+
+                pstmt.setString(1, user.getId());
+                pstmt.setString(2, user.getName());
+                pstmt.setString(3, user.getPassword());
+
+                return pstmt;
+            }
+        };
+
+        jdbcContextWithStatementStrategy( strategy );
+    }
+
+    public void deleteAll() throws SQLException, ClassNotFoundException {
+
+        StatementStrategy strategy = new StatementStrategy() {
+            @Override
+            public PreparedStatement makeStatement(Connection conn) throws SQLException {
+                return conn.prepareStatement("DELETE FROM users");
+            }
+        };
+
+        jdbcContextWithStatementStrategy( strategy );
+    }
+
+
+}
