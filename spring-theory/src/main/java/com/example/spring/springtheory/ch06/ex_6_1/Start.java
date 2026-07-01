5d45d8df093c9f2258d094e8c93f4743b33c64eb
DongWoonKim <01030985843z@naver.com>
2026-07-01 09:12:00 +0900

    spring theory ch06

--- spring-theory/src/main/java/com/example/spring/springtheory/ch06/ex_6_1/Start.java
+++ spring-theory/src/main/java/com/example/spring/springtheory/ch06/ex_6_1/Start.java
@@ -1,6 +1,13 @@
 package com.example.spring.springtheory.ch06.ex_6_1;
 
+// * AOP(Aspect Oriented Programming, 과점지향 프로그래밍)
+// 여러 모듈에서 '공통적으로 흩어져 있는 부가기능'(예: 트랜잭션, 로깅, 보안)을
+// '핵심 비즈니스 로직'에서 떼어내, 한곳에 모아 관리하는 프로그래밍 방법이다.
+// - 핵심 기능 : 그 객체가 진짜 해야 하는 일(예: 레벨 업그레이드 규칙)
+// - 부가 기능 : 여러 곳에서 똑같이 필요한 보조 기능(예: 트랜잭션 경계)
 
+// * 문제점
+// 
 
 public class Start {
     static void main(String[] args) {
