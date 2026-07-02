b3b72864312d7631cf7478c11eba3ba373cfd5aa
DongWoonKim <01030985843z@naver.com>
2026-07-02 12:17:19 +0900

    essentials

---
+++ springboot/essentials/src/main/java/com/example/spring/essentials/controller/FilterController.java
@@ -0,0 +1,9 @@
+package com.example.spring.essentials.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+// * 디스패처 서블릿(DispatcherServlet)
+// * @Controller 와 @RestController의 차이
+@RestController
+public class FilterController {
+}
