d383df1c51d493aa368bcd9ccfcf11232f47c85e
DongWoonKim <01030985843z@naver.com>
2026-07-03 10:28:49 +0900

    feign client

--- springboot/feign/feign-api/src/main/java/com/example/spring/feignapi/controller/DataController.java
+++ springboot/feign/feign-api/src/main/java/com/example/spring/feignapi/controller/DataController.java
@@ -45,7 +45,22 @@ public class DataController {
         dataStore.put(build.getId(), build);
 
         return build;
+    }
 
+    @PutMapping("/{id}")
+    public DataResponse updateData(
+            @PathVariable("id") Long id,
+            @RequestBody DataRequest dataRequest
+    ) {
+        DataResponse dataResponse = dataStore.get(id);
+
+        if (dataResponse == null) throw new RuntimeException("Data not found(update) " + id);
+
+        dataResponse.setName(dataRequest.getName());
+        dataResponse.setValue(dataRequest.getValue());
+        dataStore.put(id, dataResponse);
+
+        return dataResponse;
     }
 
 }
