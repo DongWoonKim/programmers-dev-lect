3b5b0d867fd40b4f7ce6032b5409a0606287a672
DongWoonKim <01030985843z@naver.com>
2026-07-03 10:14:12 +0900

    feign client

--- springboot/feign/feign-api/src/main/java/com/example/spring/feignapi/controller/DataController.java
+++ springboot/feign/feign-api/src/main/java/com/example/spring/feignapi/controller/DataController.java
@@ -2,12 +2,16 @@ package com.example.spring.feignapi.controller;
 
 import com.example.spring.feignapi.dto.DataResponse;
 import jakarta.annotation.PostConstruct;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/api/data")
 public class DataController {
     private Map<Long, DataResponse> dataStore = new HashMap<>();
     private Long idCounter = 1L;
@@ -19,6 +23,16 @@ public class DataController {
         dataStore.put(idCounter++, new DataResponse(3L, "Item 3", 300));
         dataStore.put(idCounter++, new DataResponse(4L, "Item 4", 400));
         dataStore.put(idCounter++, new DataResponse(5L, "Item 5", 500));
+    }
+
+    @GetMapping("/{id}")
+    public DataResponse getDataById(@PathVariable Long id) {
+
+        DataResponse dataResponse = dataStore.get(id);
+
+        if (dataResponse == null) throw new RuntimeException("Data not found " + id);
+
+        return dataResponse;
     }
 
 }
