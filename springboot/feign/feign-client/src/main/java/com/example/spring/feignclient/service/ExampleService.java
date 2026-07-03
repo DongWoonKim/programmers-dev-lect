fb684e8c67d39ba2a47ddc2569709def24becc78
DongWoonKim <01030985843z@naver.com>
2026-07-03 11:16:38 +0900

    feign client

---
+++ springboot/feign/feign-client/src/main/java/com/example/spring/feignclient/service/ExampleService.java
@@ -0,0 +1,14 @@
+package com.example.spring.feignclient.service;
+
+import com.example.spring.feignclient.client.ExampleClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ExampleService {
+
+    private final ExampleClient exampleClient;
+
+
+}
