086d9208dc189d78ea1dfccfe83a16fadd404abb
DongWoonKim <01030985843z@naver.com>
2026-07-03 09:24:52 +0900

    essentials filter

--- springboot/essentials/src/main/java/com/example/spring/essentials/controller/FilterController.java
+++ springboot/essentials/src/main/java/com/example/spring/essentials/controller/FilterController.java
@@ -1,5 +1,6 @@
 package com.example.spring.essentials.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 // * 디스패처 서블릿(DispatcherServlet) - 요청 처리의 심장
@@ -53,6 +54,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FilterController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        System.out.println("hello");
+        return "Hello World!";
+    }
+
+    @GetMapping("/api/data")
+    public String data() {
+        return "data";
+    }
+
 }
 
 
