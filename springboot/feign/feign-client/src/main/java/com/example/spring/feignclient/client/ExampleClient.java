16587b343d769685dc6944c0127420f08266f60f
DongWoonKim <01030985843z@naver.com>
2026-07-03 11:28:42 +0900

    feign client

--- springboot/feign/feign-client/src/main/java/com/example/spring/feignclient/client/ExampleClient.java
+++ springboot/feign/feign-client/src/main/java/com/example/spring/feignclient/client/ExampleClient.java
@@ -4,9 +4,9 @@ package com.example.spring.feignclient.client;
 // - interface 라서 우리가 직접 구현하지 않는다.
 // 즉 "다른 서버로 HTTP 요청을 보내는 코드"를 인터페이스 선언만으로 대신 만들어주는 것.
 
+import com.example.spring.feignclient.dto.DataRequest;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
 // * @FeignClient
 // - name : 이 클라이언트의 고유 이름(Bean 이름), 필수값
@@ -17,5 +17,14 @@ public interface ExampleClient {
 
     @GetMapping("/{id}")
     String getData(@PathVariable Long id);
+
+    @PostMapping
+    String createData(@RequestBody DataRequest dataRequest);
+
+    @PutMapping("/{id}")
+    String updateData(@PathVariable Long id, @RequestBody DataRequest dataRequest);
+
+    @DeleteMapping("/{id}")
+    String deleteData(@PathVariable Long id);
 
 }
