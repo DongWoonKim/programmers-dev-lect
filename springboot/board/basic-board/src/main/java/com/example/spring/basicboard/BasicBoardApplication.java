d941784eb2101b883d45a71485a2e5b3c59a82f3
DongWoonKim <01030985843z@naver.com>
2026-07-06 10:19:37 +0900

    basic-board set up

--- springboot/board/basic-board/src/main/java/com/example/spring/basicboard/BasicBoardApplication.java
+++ springboot/board/basic-board/src/main/java/com/example/spring/basicboard/BasicBoardApplication.java
@@ -3,6 +3,13 @@ package com.example.spring.basicboard;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// * RESTful (REST, Representational State Transfer)
+// 자원을 URI로 표현하고, HTTP메서드로 그 자원 대한 행위를 표현하는 API설계 원칙이다.
+// controller의 매핑을 아래 규칙에 맞춰 설계하면 RESTful하다고 한다.
+
+// * 핵심원칙
+// 
+
 @SpringBootApplication
 public class BasicBoardApplication {
 
