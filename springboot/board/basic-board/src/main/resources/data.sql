7dc9edef440ca8787114ed82f07a25dfd890f66b
DongWoonKim <01030985843z@naver.com>
2026-07-06 10:10:21 +0900

    basic-board set up

--- springboot/board/basic-board/src/main/resources/data.sql
+++ springboot/board/basic-board/src/main/resources/data.sql
@@ -1,3 +1,11 @@
 CREATE DATABASE java_basic
 CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
+
+-- member table
+CREATE TABLE member (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    user_id VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    user_name VARCHAR(20) NOT NULL
+);
