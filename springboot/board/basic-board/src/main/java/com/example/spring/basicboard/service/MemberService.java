a17606be2c71ba4a88d55130dc675c99ad396039
DongWoonKim <01030985843z@naver.com>
2026-07-06 11:45:55 +0900

    basic-board sign up

---
+++ springboot/board/basic-board/src/main/java/com/example/spring/basicboard/service/MemberService.java
@@ -0,0 +1,22 @@
+package com.example.spring.basicboard.service;
+
+import com.example.spring.basicboard.domain.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+// 이 클래스의 "모든 메서드"에 기본 적용된다.
+// - readOnly = true의 효과
+// "이 트랜잭션은 데이터를 안 바꾼다."라고 JPA한테 알려준다. -> 최적화
+// 하이버네이트가 변경감지를 위한 스냅샷을 안 만들어 메모리/성능에 유리
+// Insert/Update/Delete가 필요한 메서드는 @Transactional을 다시 붙인다.
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    
+
+}
