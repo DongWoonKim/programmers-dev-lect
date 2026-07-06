5dc168cfd7c927f9451091e239e8d6fa427a4caf
DongWoonKim <01030985843z@naver.com>
2026-07-06 12:22:55 +0900

    basic-board sign up

--- springboot/board/basic-board/src/main/java/com/example/spring/basicboard/domain/repository/MemberRepository.java
+++ springboot/board/basic-board/src/main/java/com/example/spring/basicboard/domain/repository/MemberRepository.java
@@ -3,6 +3,8 @@ package com.example.spring.basicboard.domain.repository;
 import com.example.spring.basicboard.domain.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 // 이 인터페이스에는 구현 클래스가 없다.
 // - 우리는 inerface만 선언하고 구현체(class)는 만들지 않는다.
 // - 애플리케이션이 뜰 때 Spring Data JPA 가 이 인터페이스의 구현체를 프록시로 자동 생성해서 빈으로 등록한다.
@@ -20,5 +22,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     boolean existsByUserId(String userId);
+
+    Optional<Member> findByUserId(String userId);
 
 }
