423b93da3af56741eac794f2ac42935c07a9cbbb
DongWoonKim <01030985843z@naver.com>
2026-07-06 12:28:47 +0900

    basic-board sign up

--- springboot/board/basic-board/src/main/java/com/example/spring/basicboard/service/MemberService.java
+++ springboot/board/basic-board/src/main/java/com/example/spring/basicboard/service/MemberService.java
@@ -1,5 +1,6 @@
 package com.example.spring.basicboard.service;
 
+import com.example.spring.basicboard.domain.entity.Member;
 import com.example.spring.basicboard.domain.repository.MemberRepository;
 import com.example.spring.basicboard.dto.MemberJoinRequestDto;
 import com.example.spring.basicboard.exception.DuplicateUserIdException;
@@ -21,13 +22,13 @@ public class MemberService {
     private final MemberRepository memberRepository;
     private final MemberMapper memberMapper;
 
+    @Transactional
     public void join(MemberJoinRequestDto dto) {
         // 아이디 중복체크
         if ( memberRepository.existsByUserId(dto.getUserId()) ) {
             // 예외 공통화
             throw new DuplicateUserIdException("[회원가입] 이미 존재하는 아이디입니다.");
         }
-
         memberRepository.save( memberMapper.toEntity(dto) );
     }
 
