b976a78546bddd05df952c261811c5a0272bae22
DongWoonKim <01030985843z@naver.com>
2026-07-07 11:21:46 +0900

    basic board board-list

---
+++ springboot/board/basic-board/src/main/java/com/example/spring/basicboard/controller/BoardApiController.java
@@ -0,0 +1,11 @@
+package com.example.spring.basicboard.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/boards")
+public class BoardApiController {
+}
