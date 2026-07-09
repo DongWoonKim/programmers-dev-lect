261e164f89d853bfa6c55e10b80e6c2cb61a36c4
DongWoonKim <01030985843z@naver.com>
2026-07-09 10:05:27 +0900

    basic-board aop

--- springboot/board/basic-board/src/main/java/com/example/spring/basicboard/aop/LogginAspect.java
+++ springboot/board/basic-board/src/main/java/com/example/spring/basicboard/aop/LogginAspect.java
@@ -1,10 +1,16 @@
 package com.example.spring.basicboard.aop;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.util.Arrays;
 
 // * @Aspect
 // - "이 클래스는 공통 기능(횡단 관심사)을 모아둔 Aspect다"라고 선언하는 어노테이션이다.
@@ -37,7 +43,40 @@ public class LogginAspect {
     // - @Around          : 대상 메서드 실행을 "통째로 감싼다". 전/후/예외를 모두 한 메서드에서 제어 가능
     @Around("controllerLog()")
     public Object logRequest(ProceedingJoinPoint joinPoint) throws Throwable {
-        return null;
+
+        String method = joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName();
+
+        String httpInfo = "";
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        if ( attributes != null ) {
+            HttpServletRequest request = attributes.getRequest();
+            httpInfo = request.getMethod() + " " + request.getRequestURI();
+        }
+
+        // === 대상 메서드 실행 "전" 로깅 ===
+        System.out.println("[요청 시작] " + httpInfo + " -> " + method);
+
+        System.out.println("[파라미터] " + Arrays.toString(joinPoint.getArgs()) );
+
+        long start = System.currentTimeMillis();
+
+        try {
+            Object result = joinPoint.proceed();
+
+            // === 대상 메서드가 "정상 종료"된 후 로깅 ===
+            long end = System.currentTimeMillis() - start; // 걸린시간
+            System.out.println("[요청 완료] " + method + " : " + end + "ms");
+
+            return result;
+        } catch ( Throwable e ) {
+            // === 대상 메서드가 "예외를 던졌을 때" 로깅 ===
+            long end = System.currentTimeMillis() - start; // 걸린시간
+            System.out.println("[요청 실패] " + method + " : " + end + "ms" + " : 예외메시지 " + e.getMessage());
+
+            // 잡은 예외를 다시 던진다.
+            // - 여기서 예외를 삼켜버리면 컨트롤러는 정상 처리된 것처럼 보여 버그가 된다.
+            throw e;
+        }
     }
 
 }
