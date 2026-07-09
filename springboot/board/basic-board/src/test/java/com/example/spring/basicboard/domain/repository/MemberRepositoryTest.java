5a6d74975ff52738eebed3439f68e798d300561b
DongWoonKim <01030985843z@naver.com>
2026-07-09 10:38:37 +0900

    basic-board test

---
+++ springboot/board/basic-board/src/test/java/com/example/spring/basicboard/domain/repository/MemberRepositoryTest.java
@@ -0,0 +1,7 @@
+package com.example.spring.basicboard.domain.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberRepositoryTest {
+
+}
---
+++ springboot/board/basic-board/src/test/java/com/example/spring/basicboard/service/MemberServiceTest.java
@@ -0,0 +1,7 @@
+package com.example.spring.basicboard.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+}
