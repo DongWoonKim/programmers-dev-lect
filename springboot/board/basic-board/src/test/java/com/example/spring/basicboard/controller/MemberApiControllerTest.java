2706937091272b8e68cd0f204981f23e77230ef4
DongWoonKim <01030985843z@naver.com>
2026-07-10 09:29:32 +0900

    basic-board test

--- springboot/board/basic-board/src/test/java/com/example/spring/basicboard/controller/MemberApiControllerTest.java
+++ springboot/board/basic-board/src/test/java/com/example/spring/basicboard/controller/MemberApiControllerTest.java
@@ -1,6 +1,20 @@
 package com.example.spring.basicboard.controller;
 
+import com.example.spring.basicboard.service.MemberService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import tools.jackson.databind.ObjectMapper;
+
+import java.util.Map;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 // * 프레젠테이션 계층 테스트 - 컨트롤러의 "Http 계약"을 검증한다
 
@@ -15,4 +29,58 @@ import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 @WebMvcTest(MemberApiController.class)
 class MemberApiControllerTest {
 
+    // MockMvc : 실제 서버(톰캣)를 띄우지 않고, HTTP 요청을 "흉내 내서" 컨트롤러에 넣어보는 도구
+    @Autowired
+    private MockMvc mockMvc;
+
+    // ObjectMapper : 객체를 JSON으로 변환할 때 쓴다.
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockitoBean
+    private MemberService memberService;
+
+    @Test
+    @DisplayName("회원가입 성공 - 200과 이동할 url을 반환한다.")
+    void join_성공() throws Exception {
+        // given
+        String requestJson = objectMapper.writeValueAsString(
+                Map.of(
+                        "userId", "test",
+                        "password", "1234",
+                        "name", "test"
+                )
+        );
+
+        // when & then
+        mockMvc.perform(
+                post("/api/members/join")
+                        .contentType( MediaType.APPLICATION_JSON )
+                        .content( requestJson )
+        )
+        .andExpect(status().isOk())
+        .andExpect( jsonPath("$.url").value("/members/login") );
+
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
