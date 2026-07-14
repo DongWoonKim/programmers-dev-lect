51242aef2e121fa83dcc90182518843fec4f1e6f
DongWoonKim <01030985843z@naver.com>
2026-07-14 10:36:00 +0900

    basic-board detail

--- springboot/board/basic-board/src/main/java/com/example/spring/basicboard/controller/CommentApiController.java
+++ springboot/board/basic-board/src/main/java/com/example/spring/basicboard/controller/CommentApiController.java
@@ -1,14 +1,19 @@
 package com.example.spring.basicboard.controller;
 
 import com.example.spring.basicboard.dto.CommentWriteRequestDto;
+import com.example.spring.basicboard.service.CommentService;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "댓글 API", description = "게시글에 댓글 관련")
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/boards/{boardId}/comments")
 public class CommentApiController {
+
+    private final CommentService commentService;
 
     @PostMapping
     public void addComment(
@@ -16,7 +21,7 @@ public class CommentApiController {
             @PathVariable long boardId,
             @RequestBody CommentWriteRequestDto dto
     ) {
-
+        commentService.addComment(boardId, dto);
     }
 
 }
