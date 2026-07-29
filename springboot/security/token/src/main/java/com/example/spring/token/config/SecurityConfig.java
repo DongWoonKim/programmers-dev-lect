b234852864e3f2b73f64d69fc98aa1cc2002ea02
DongWoonKim <01030985843z@naver.com>
2026-07-29 10:36:58 +0900

    spring-security token

--- springboot/security/token/src/main/java/com/example/spring/token/config/SecurityConfig.java
+++ springboot/security/token/src/main/java/com/example/spring/token/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 // * JWT(JSON Web Token)
@@ -97,5 +98,9 @@ public class SecurityConfig {
         return http.build();
     }
 
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
 }
