5f89ebd8e258222f4bd640619fd460f048157837
DongWoonKim <01030985843z@naver.com>
2026-07-30 10:47:08 +0900

    spring-security token

--- springboot/security/token/src/main/java/com/example/spring/token/config/SecurityConfig.java
+++ springboot/security/token/src/main/java/com/example/spring/token/config/SecurityConfig.java
@@ -100,6 +100,7 @@ public class SecurityConfig {
                                 "/", // 페이지(HTML)는 공개, 데이터는 보호 - 브라우저 페이지 이동은 Bearer 헤더를 못 실으므로 페이지 인가는 API가 담당
                                 "/api/users/login",
                                 "/api/users/join",
+                                "/api/tokens/refresh",
 
                                 "/css/**",
                                 "/js/**"
