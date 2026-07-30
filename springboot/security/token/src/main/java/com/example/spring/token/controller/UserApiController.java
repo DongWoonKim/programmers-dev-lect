06700a0094ac75fd0f0d98e050113219bd3634bc
DongWoonKim <01030985843z@naver.com>
2026-07-30 10:09:24 +0900

    spring-security token

--- springboot/security/token/src/main/java/com/example/spring/token/controller/UserApiController.java
+++ springboot/security/token/src/main/java/com/example/spring/token/controller/UserApiController.java
@@ -1,10 +1,13 @@
 package com.example.spring.token.controller;
 
+import com.example.spring.token.config.jwt.JwtProperties;
 import com.example.spring.token.dto.SignInRequestDto;
 import com.example.spring.token.dto.SignInResponseDto;
 import com.example.spring.token.dto.SignUpRequestDto;
 import com.example.spring.token.dto.SignUpResponseDto;
 import com.example.spring.token.service.UserService;
+import com.example.spring.token.util.CookieUtil;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserApiController {
 
     private final UserService userService;
+    private final JwtProperties jwtProperties;
 
     @PostMapping("/join")
     public SignUpResponseDto join(@RequestBody SignUpRequestDto requestDto ) {
@@ -34,9 +38,16 @@ public class UserApiController {
     ) {
         SignInResponseDto signInResponseDto = userService.login(requestDto);
 
+        CookieUtil.addCookie(
+                response,
+                CookieUtil.REFRESH_TOKEN_COOKIE,
+                signInResponseDto.getRefreshToken(),
+                (int) jwtProperties.getRefreshTokenValidity().toSeconds()
+        );
 
+        signInResponseDto.setAccessToken(null);
 
-        return null;
+        return signInResponseDto;
     }
 
 }
