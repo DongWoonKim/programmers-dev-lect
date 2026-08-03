b5668c59ed23413634d042d9689e77bba3eaa551
DongWoonKim <01030985843z@naver.com>
2026-08-03 10:51:15 +0900

    spring-security oauth2 : login/out set up

--- springboot/security/oauth2/src/main/java/com/example/spring/oauth2/controller/UserApiController.java
+++ springboot/security/oauth2/src/main/java/com/example/spring/oauth2/controller/UserApiController.java
@@ -1,8 +1,11 @@
 package com.example.spring.oauth2.controller;
 
-import com.example.spring.oauth2.dto.SignUpRequestDto;
-import com.example.spring.oauth2.dto.SignUpResponseDto;
+import com.example.spring.oauth2.config.jwt.JwtProperties;
+import com.example.spring.oauth2.dto.*;
 import com.example.spring.oauth2.service.UserService;
+import com.example.spring.oauth2.util.CookieUtil;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,11 +18,43 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserApiController {
 
     private final UserService userService;
+    private final JwtProperties jwtProperties;
 
     @PostMapping("/join")
     public SignUpResponseDto join(@RequestBody SignUpRequestDto requestDto ) {
         userService.signUp(requestDto);
         return SignUpResponseDto.builder()
+                .url("/users/login")
+                .build();
+    }
+
+    @PostMapping("/login")
+    public SignInResponseDto login(
+            @RequestBody SignInRequestDto requestDto,
+            HttpServletResponse response
+    ) {
+        SignInResponseDto signInResponseDto = userService.login(requestDto);
+
+        CookieUtil.addCookie(
+                response,
+                CookieUtil.REFRESH_TOKEN_COOKIE,
+                signInResponseDto.getRefreshToken(),
+                (int) jwtProperties.getRefreshTokenValidity().toSeconds()
+        );
+
+        signInResponseDto.setRefreshToken(null);
+
+        return signInResponseDto;
+    }
+
+    @PostMapping("/logout")
+    public LogoutResponseDto logout(
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) {
+        CookieUtil.deleteCookie(request, response, CookieUtil.REFRESH_TOKEN_COOKIE);
+        return LogoutResponseDto.builder()
+                .message("로그아웃 되었습니다.")
                 .url("/users/login")
                 .build();
     }
