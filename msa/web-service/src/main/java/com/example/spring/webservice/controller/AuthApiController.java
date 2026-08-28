b5e3553aaa124ff1ccfc43b27140b8eb6cf11a6d
DongWoonKim <01030985843z@naver.com>
2026-08-28 10:12:03 +0900

    msa : auth-service

--- msa/web-service/src/main/java/com/example/spring/webservice/controller/AuthApiController.java
+++ msa/web-service/src/main/java/com/example/spring/webservice/controller/AuthApiController.java
@@ -1,8 +1,12 @@
 package com.example.spring.webservice.controller;
 
+import com.example.spring.webservice.dto.SignInRequestDto;
+import com.example.spring.webservice.dto.SignInResponseDto;
 import com.example.spring.webservice.dto.SignUpRequestDto;
 import com.example.spring.webservice.dto.SignUpResponseDto;
 import com.example.spring.webservice.service.AuthService;
+import com.example.spring.webservice.util.HeaderRelayUtil;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,6 +23,14 @@ public class AuthApiController {
     @PostMapping("/join")
     public SignUpResponseDto join(@RequestBody SignUpRequestDto signUpRequestDto) {
         return authService.signUp(signUpRequestDto);
+    }
+
+    @PostMapping("/login")
+    public SignInResponseDto login(
+            @RequestBody SignInRequestDto signInRequestDto,
+            HttpServletResponse response
+    ) {
+        return HeaderRelayUtil.relaySetCookie(authService.signIn(signInRequestDto), response);
     }
 
 }
--- msa/web-service/src/main/java/com/example/spring/webservice/service/AuthService.java
+++ msa/web-service/src/main/java/com/example/spring/webservice/service/AuthService.java
@@ -1,9 +1,12 @@
 package com.example.spring.webservice.service;
 
 import com.example.spring.webservice.client.AuthClient;
+import com.example.spring.webservice.dto.SignInRequestDto;
+import com.example.spring.webservice.dto.SignInResponseDto;
 import com.example.spring.webservice.dto.SignUpRequestDto;
 import com.example.spring.webservice.dto.SignUpResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,6 +17,10 @@ public class AuthService {
 
     public SignUpResponseDto signUp(SignUpRequestDto signUpRequestDto) {
         return authClient.join(signUpRequestDto);
+    }
+
+    public ResponseEntity<SignInResponseDto> signIn(SignInRequestDto signInRequestDto) {
+        return authClient.login(signInRequestDto);
     }
 
 }
