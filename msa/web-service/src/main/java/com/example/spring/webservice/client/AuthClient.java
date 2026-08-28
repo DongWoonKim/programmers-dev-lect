f24923c0c3687f123ad506d57955252f92e2211d
DongWoonKim <01030985843z@naver.com>
2026-08-28 10:08:52 +0900

    msa : auth-service

--- msa/web-service/src/main/java/com/example/spring/webservice/client/AuthClient.java
+++ msa/web-service/src/main/java/com/example/spring/webservice/client/AuthClient.java
@@ -1,15 +1,24 @@
 package com.example.spring.webservice.client;
 
+import com.example.spring.webservice.dto.SignInRequestDto;
+import com.example.spring.webservice.dto.SignInResponseDto;
 import com.example.spring.webservice.dto.SignUpRequestDto;
 import com.example.spring.webservice.dto.SignUpResponseDto;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+// ResponseEntity로 받는 메서드들
+// auth-service가 refresh token을 Set-Cookie 헤더로 내려주는 응답과
+// 쿠키를 지우는 응답은 본문만 받으면 헤더가 유실된다.
+// ResponseEntity로 받아 컨트롤러가 Set-Cookie를 브라우저 응답에 옮겨 실어야한다.
 @FeignClient(value = "auth-service", url = "${edge-service.url:http://localhost:8000}")
 public interface AuthClient {
 
     @PostMapping("/api/users/join")
     SignUpResponseDto join(@RequestBody SignUpRequestDto signUpRequestDto);
 
+    @PostMapping("/api/users/login")
+    ResponseEntity<SignInResponseDto> login(@RequestBody SignInRequestDto signInRequestDto);
 }
