4c98460e551f956f7b2ac483dc87e89df009112f
DongWoonKim <01030985843z@naver.com>
2026-08-28 09:49:34 +0900

    msa : auth-service

--- msa/auth-service/src/main/java/com/example/spring/authservice/config/SecurityConfig.java
+++ msa/auth-service/src/main/java/com/example/spring/authservice/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.spring.authservice.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -12,7 +13,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-//@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableMethodSecurity(prePostEnabled = true)
 @RequiredArgsConstructor
 public class SecurityConfig {
 
