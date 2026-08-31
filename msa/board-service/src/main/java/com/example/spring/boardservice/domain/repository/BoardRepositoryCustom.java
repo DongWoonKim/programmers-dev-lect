e012f7c836296776c50e8b3a25ecadfecbff4c26
DongWoonKim <01030985843z@naver.com>
2026-08-31 12:44:49 +0900

    msa : searchBoards

--- msa/board-service/src/main/java/com/example/spring/boardservice/domain/repository/BoardRepositoryCustom.java
+++ msa/board-service/src/main/java/com/example/spring/boardservice/domain/repository/BoardRepositoryCustom.java
@@ -1,4 +1,12 @@
 package com.example.spring.boardservice.domain.repository;
 
+import com.example.spring.boardservice.dto.BoardListItemResponseDto;
+import com.example.spring.boardservice.dto.BoardSearchRequestDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface BoardRepositoryCustom {
+
+    Page<BoardListItemResponseDto> searchBoards(BoardSearchRequestDto condition, Pageable pageable);
+
 }
--- msa/board-service/src/main/java/com/example/spring/boardservice/domain/repository/BoardRepositoryImpl.java
+++ msa/board-service/src/main/java/com/example/spring/boardservice/domain/repository/BoardRepositoryImpl.java
@@ -1,4 +1,21 @@
 package com.example.spring.boardservice.domain.repository;
 
+import com.example.spring.boardservice.dto.BoardListItemResponseDto;
+import com.example.spring.boardservice.dto.BoardSearchRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
 public class BoardRepositoryImpl implements BoardRepositoryCustom {
+
+
+
+    @Override
+    public Page<BoardListItemResponseDto> searchBoards(BoardSearchRequestDto condition, Pageable pageable) {
+
+        return null;
+    }
 }
