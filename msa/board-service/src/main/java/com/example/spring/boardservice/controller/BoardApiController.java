8ba09bdeaa7637fbe7b2d21e50f26c71456168dc
DongWoonKim <01030985843z@naver.com>
2026-08-31 11:34:43 +0900

    msa : board-service

--- msa/board-service/src/main/java/com/example/spring/boardservice/controller/BoardApiController.java
+++ msa/board-service/src/main/java/com/example/spring/boardservice/controller/BoardApiController.java
@@ -2,6 +2,7 @@ package com.example.spring.boardservice.controller;
 
 import com.example.spring.boardservice.service.BoardService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class BoardApiController {
 
     private final BoardService boardService;
+
+    @GetMapping("/search")
+    public void searchBoards() {
+
+    }
+
 
 }
