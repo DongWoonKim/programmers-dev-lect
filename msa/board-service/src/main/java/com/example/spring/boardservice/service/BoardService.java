3bbeabd1339940ac8c6bf6ecd60991d90c2d2975
DongWoonKim <01030985843z@naver.com>
2026-09-01 09:46:24 +0900

    msa : searchBoards

--- msa/board-service/src/main/java/com/example/spring/boardservice/service/BoardService.java
+++ msa/board-service/src/main/java/com/example/spring/boardservice/service/BoardService.java
@@ -1,7 +1,11 @@
 package com.example.spring.boardservice.service;
 
 import com.example.spring.boardservice.domain.repository.BoardRepository;
+import com.example.spring.boardservice.dto.BoardListItemResponseDto;
+import com.example.spring.boardservice.dto.BoardSearchRequestDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,12 +16,15 @@ public class BoardService {
 
     private final BoardRepository boardRepository;
 
-    public void searchBoards() {
+    public Page<BoardListItemResponseDto> searchBoards(BoardSearchRequestDto dto, Pageable pageable) {
 
         // searchBoards 게시글들 가져오기
+        Page<BoardListItemResponseDto> page = boardRepository.searchBoards(dto, pageable);
 
         // boardRepository에서 가져온 ID추려서 auth-service로 요청해서 userName들 받아오기
 
+
+        return null;
     }
 
 
