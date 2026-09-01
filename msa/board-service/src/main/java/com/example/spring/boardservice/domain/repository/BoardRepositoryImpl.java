f64492c48eabaa866546f02f4dada170ad4411cf
DongWoonKim <01030985843z@naver.com>
2026-09-01 09:38:01 +0900

    msa : searchBoards

--- msa/board-service/src/main/java/com/example/spring/boardservice/domain/repository/BoardRepositoryImpl.java
+++ msa/board-service/src/main/java/com/example/spring/boardservice/domain/repository/BoardRepositoryImpl.java
@@ -10,10 +10,12 @@ import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.JPAExpressions;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -53,10 +55,20 @@ public class BoardRepositoryImpl implements BoardRepositoryCustom {
                 .orderBy(board.id.desc())
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
-                .fetch()
-                ;
+                .fetch();
 
-        return null;
+        // 전체 개수 쿼리
+        JPAQuery<Long> countQuery = queryFactory
+                .select(board.count())
+                .from(board)
+                .where(
+                        titleContains(condition.getTitle()),
+                        userIdEquals(condition.getUserId()),
+                        createdGoe(condition.getFrom()),
+                        createdLoe(condition.getTo())
+                );
+
+        return PageableExecutionUtils.getPage(content, pageable, countQuery::fetchOne);
     }
 
     // 제목 부분 일치 (Like %title%). 빈 값이면 조건 없음(null)
