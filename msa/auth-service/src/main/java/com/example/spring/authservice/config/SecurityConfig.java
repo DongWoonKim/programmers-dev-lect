c27e1b609ec978aafed423eee9d3209237491a5d
DongWoonKim <01030985843z@naver.com>
2026-09-07 10:17:46 +0900

    msa : oauth2

--- msa/auth-service/src/main/java/com/example/spring/authservice/config/SecurityConfig.java
+++ msa/auth-service/src/main/java/com/example/spring/authservice/config/SecurityConfig.java
@@ -1,6 +1,9 @@
 package com.example.spring.authservice.config;
 
 import com.example.spring.authservice.config.filter.TokenAuthenticationFilter;
+import com.example.spring.authservice.config.oauth2.OAuth2FailureHandler;
+import com.example.spring.authservice.config.oauth2.OAuth2SuccessHandler;
+import com.example.spring.authservice.service.CustomOAuth2UserService;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -29,6 +32,10 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final CustomOAuth2UserService customOAuth2UserService;
+    private final OAuth2SuccessHandler oAuth2SuccessHandler;
+    private final OAuth2FailureHandler oAuth2FailureHandler;
+
     @Value("${web-service.url}")
     private String webServiceUrl;
     private final TokenAuthenticationFilter tokenAuthenticationFilter;
@@ -52,6 +59,15 @@ public class SecurityConfig {
                                 "/error"
                         ).permitAll()
                         .anyRequest().authenticated()
+                )
+                .oauth2Login(
+                        oauth2 -> oauth2
+                                .userInfoEndpoint(
+                                        userInfo -> userInfo
+                                                .userService(customOAuth2UserService)
+                                )
+                                .successHandler(oAuth2SuccessHandler)
+                                .failureHandler(oAuth2FailureHandler)
                 )
                 .addFilterBefore(tokenAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .exceptionHandling(exception ->  exception
