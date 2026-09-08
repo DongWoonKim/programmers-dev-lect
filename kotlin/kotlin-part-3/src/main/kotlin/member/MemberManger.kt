670316fb9420973f9a1c42725fa4dee4dac04846
DongWoonKim <01030985843z@naver.com>
2026-09-08 17:11:55 +0900

    member : MemberManger

--- kotlin/kotlin-part-3/src/main/kotlin/member/MemberManger.kt
+++ kotlin/kotlin-part-3/src/main/kotlin/member/MemberManger.kt
@@ -1,4 +1,15 @@
 package member
 
-class MemberManger {
+class MemberManger(planNo: Int) {
+
+    val totalCnt = planNo * 10
+    // val : 요소를 추가하고 삭제할 수 있다. 단 주소참조 변경은 불가능
+    private val members = mutableListOf<Member>()
+    // 커스텀 getter : 값을 저장해 두지 않고 읽을 때마다 계산한다
+    val memberCnt: Int
+        get() = members.size
+    val isFull: Boolean
+        get() = totalCnt <= members.size    
+
+
 }
