25033157bf8127a03c08f9155f0f8e1f8804a433
DongWoonKim <01030985843z@naver.com>
2026-09-10 09:10:15 +0900

    kotlin-part-4 : prj setup

---
+++ kotlin/kotlin-part-4/src/test/kotlin/com/example/spring/kotlinpart4/KotlinPart4ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.spring.kotlinpart4
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinPart4ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
