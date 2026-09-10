ab03311676aba16ddd17c8802aa682b46ef04373
DongWoonKim <01030985843z@naver.com>
2026-09-10 12:14:27 +0900

    kotlin-part-3 : member

--- kotlin/kotlin-part-3/src/main/kotlin/member/MemberApp.kt
+++ kotlin/kotlin-part-3/src/main/kotlin/member/MemberApp.kt
@@ -1,12 +1,13 @@
 package member
 
-class MemberApp(private val memberManager: MemberManager) {
+class MemberApp(private val manager: MemberManager) {
 
     fun start() {
 
         while (true) {
 
             when(printMenu()) {
+                1 -> addMember()
                 9 -> {
                     println("이용해주셔서 감사합니다.")
                     return
@@ -20,7 +21,7 @@ class MemberApp(private val memberManager: MemberManager) {
 
     private fun printMenu(): Int {
 
-        println("[수행할 업무를 선택하세요. - 현재 회원수 : ${memberManager.memberCnt} / ${memberManager.totalCnt}]")
+        println("[수행할 업무를 선택하세요. - 현재 회원수 : ${manager.memberCnt} / ${manager.totalCnt}]")
         println("[1]회원추가 [2]회원조회(메일) [3]회원조회(이름)")
         println("[4]전체조회 [5]정보수정 [6]회원삭제")
         println("[7]이름검색 [8]도메인별통계 [9]프로그램 종료")
@@ -28,4 +29,48 @@ class MemberApp(private val memberManager: MemberManager) {
         return readln().toInt()
     }
 
+    private fun addMember() {
+
+        if ( manager.isFull ) {
+            println("회원이 꽉 찼습니다.")
+            return
+        }
+
+        println("이름을 입력하세요.")
+        val name = readln()
+        println("이메일을 입력하세요.")
+        val email = readln()
+        println("연락처를 입력하세요.")
+        val phone = readln()
+
+        if ( manager.addMember( Member(name, email, phone) ) ) {
+            println("회원이 등록되었습니다.")
+        } else {
+            println("이미 존재하는 회원입니다.")
+        }
+
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
