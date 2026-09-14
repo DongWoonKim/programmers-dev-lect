4d63f64b4dc16368597ef2873211a52df06d6ff8
DongWoonKim <01030985843z@naver.com>
2026-09-14 11:25:17 +0900

    updated

--- msa/auth-service/src/main/java/com/example/spring/authservice/config/client/BoardClient.java
+++ msa/auth-service/src/main/java/com/example/spring/authservice/config/client/BoardClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(value = "board-service", url = "${board-service.url}:http://localhost:8081")
+@FeignClient(value = "board-service", url = "${board-service.url:http://localhost:8081}")
 public interface BoardClient {
 
     @DeleteMapping("/api/boards/internal/users/{userId}")
